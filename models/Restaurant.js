548b02449ec0371e41a02acc28284aeca88d9d08
davidcarlin <david.carlin@barclays.com>
2023-02-17 08:32:35 +0000

    restaurant model defined

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -2,5 +2,10 @@ const {sequelize} = require('../db');
 const { Sequelize } = require('sequelize');
 
 // TODO - create a Restaurant model
+const Restaurant = sequelize.define("Restaurant", {
+  name: Sequelize.STRING,
+  location: Sequelize.STRING,
+  cuisine: Sequelize.STRING,
+});
 
 module.exports = {Restaurant};
